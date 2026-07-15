771c78e6932b836da9cd64b2c8dbaa79f5051971
Joao <joao@example.com>
2026-07-15 10:06:41 -0300

    criaçao do middleware que verifica se a requisiçao tem o reader

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,7 +1,5 @@
 const { verifyToken } = require('../utils/jwt');
 
-// Verifica se o header Authorization tem um token JWT válido.
-// Se válido, coloca os dados do usuário em req.user e segue para a rota.
 function authMiddleware(req, res, next) {
   const authHeader = req.headers['authorization'];
 
